6ac36148dae5661495ded19888e67f41bb5f057e
Daniel <dmartingar@uoc.edu>
2014-05-16 03:57:09 +0200

    Create Mamifero.java

---
+++ Mamifero.java
@@ -0,0 +1,19 @@
+package beans;
+
+
+public class Mamifero extends Mascota {
+    
+        public String tipoMamifero;//granja o exótico
+
+    public Mamifero(String chip, String nombre, String estadoFisico, String estadoMental) {
+        super(chip, nombre, estadoFisico, estadoMental);
+        this.tipoMamifero = tipoMamifero;
+        
+    }
+
+   
+
+   
+    
+ 
+}//cierra la clase Mamifero
