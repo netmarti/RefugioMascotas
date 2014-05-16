af43a413f944f27439d781d58b300d9aafc434a0
Daniel <dmartingar@uoc.edu>
2014-05-16 03:59:05 +0200

    Update Ave.java

--- Ave.java
+++ Ave.java
@@ -3,47 +3,26 @@
  */
 package beans;
 
-/**
- *Clase Ave
- */
+
 public class Ave extends Mascota {
+    
+        public String tipoAve;//cantora o parlanchina
 
-  private String comportamiento;
-  
-    /*
-     * @param protected String chip;
-     * @param protected String tipo;
-     * @param protected String nombre;
-     * @param protected int edad;
-     * @param protected Estado estado; // fisico y mental
-     * @param listaMascotas
-     * @param ArrayList
-     * @param comportamiento // diurno o nocturno
-     */
-     
-      //constructor de la clase Ave
-  
-    public Ave(String chip, String tipo, String nombre, 
-    int edad, String fisico, String mental, java.util.
-    ArrayList<Mascota> listaMascotas, Mascota ArrayList, String comportamiento) {
-        
-        super(chip,tipo,nombre,edad,fisico,mental);
-        this.comportamiento = comportamiento;
-     }
-
-  //seters y geters para obtener la variable Comportamiento
-  
-    public String getComportamiento() {
-        return comportamiento;
+    //creamos el constructor
+    public Ave(String tipoAve, String chip, String nombre, String estadoFisico, String estadoMental) {
+        super(chip, nombre, estadoFisico, estadoMental);
+        this.tipoAve = tipoAve;
+    }
+   
+    //getters y setters
+    public String getTipoAve() {
+        return tipoAve;
     }
 
-    public void setComportamiento(String comportamiento) {
-        this.comportamiento = comportamiento;
+    public void setTipoAve(String tipoAve) {
+        this.tipoAve = tipoAve;
     }
-
-    @Override
-    public String toString() {
-        return "Ave{" + "comportamiento=" + comportamiento + '}';
-    }
-
-}//cierra la clase, útlima llave del documento
+    
+    
+    
+}//cierra la clase Ave
